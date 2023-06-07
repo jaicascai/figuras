58ae868486e356219a3bf6d366437e2bc90e655e
jaicascai <135773745+jaicascai@users.noreply.github.com>
2023-06-06 19:50:52 -0500

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,60 @@
+function circulo(){
+    let figura = document.getElementById("figura");
+    figura.classList.toggle("CIRCULO");
+
+}
+function rectangulo(){
+    let figura = document.getElementById("figura");
+    figura.classList.toggle("RECTANGULO");
+
+}
+function triangulo(){
+    let figura = document.getElementById("figura");
+    figura.classList.toggle("TRIANGULO");
+
+}
+function girar(){
+    let figura = document.getElementById("figura");
+    figura.classList.toggle("GIRAR");
+
+}
+function arriba(){
+    let figura = document.getElementById("figura");
+    figura.classList.toggle("ARRIBA");
+
+}
+function abajo(){
+    let figura = document.getElementById("figura");
+    figura.classList.toggle("ABAJO");
+
+}
+function izquierda(){
+    let figura = document.getElementById("figura");
+    figura.classList.toggle("IZQUIERDA");
+
+}
+function derecha(){
+    let figura = document.getElementById("figura");
+    figura.classList.toggle("DERECHA");
+}
+function ocultar(){
+    let figura = document.getElementById("figura");
+    figura.classList.toggle("OCULTAR");
+}
+function gif(){
+    let figura = document.getElementById("figura");
+    figura.classList.toggle("GIF");
+    
+}
+function degradado(){
+    let figura = document.getElementById("figura");
+    figura.classList.toggle("DEGRADADO");
+}
+function panel(){
+    const panel = document.querySelector(".paneliz");
+    panel.classList.toggle("activo");
+}
+function panelsup(){
+    const panel = document.querySelector(".panelsup");
+    panel.classList.toggle("activo");
+}
